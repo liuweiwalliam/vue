5f3c8449f9642228b2924a02348a8534de656320
liuweiwalliam <liuweiwalliam@163.com>
2017-07-01 09:38:50 +0800

    tets

--- vueCli/myVue/src/App.vue
+++ vueCli/myVue/src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view></router-view>
+    <ul>
+      <li  v-for="(value,index) in items">{{item.label}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    data: function () {
+      return {
+        tittle: 'this is a todo list',
+        items: [
+          {label: 'task1', isfinished: false},
+          {label: 'task2', isfinished: true},
+          {label: 'task3', isfinished: true}]
+      }
+    },
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
